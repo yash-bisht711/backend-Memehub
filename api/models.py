fbfe007698f130d84c09ac3cea1b94fe7428a1fd
theDAOS <sanjusanthosh46@gmail.com>
2025-05-16 03:13:12 +0530

    rm -a

--- api/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Item(models.Model):
-    name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
