8d548bcfa9987a5e8ca50375894c6c2530072990
theDAOS <sanjusanthosh46@gmail.com>
2025-05-16 03:20:59 +0530

    Initialize Django project structure with API app and basic configurations

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+from .views import ItemViewSet
+from django.urls import path, include
+
+router = DefaultRouter()
+router.register(r'items', ItemViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
