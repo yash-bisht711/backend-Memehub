81376dee9c319ebcbad4c341679ace76140c4abb
theDAOS <sanjusanthosh46@gmail.com>
2025-05-16 04:29:54 +0530

    Remove Item model and update User_profile to include avatar_url property and serializer method

--- api/models.py
+++ api/models.py
@@ -1,13 +1,5 @@
 from django.db import models
 
-class Item(models.Model):
-    name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
-
 class User_profile(models.Model):
     email = models.EmailField(primary_key=True)
     name = models.CharField(max_length=100)
@@ -16,3 +8,9 @@ class User_profile(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.email})"
+
+    @property
+    def avatar_url(self):
+        if self.profile_picture:
+            return self.profile_picture.url
+        return f"https://api.dicebear.com/9.x/micah/svg?seed={self.handle or self.email}"
--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import User_profile
 
-
 class UserSerializer(serializers.ModelSerializer):
+    avatar_url = serializers.SerializerMethodField()
+
     class Meta:
         model = User_profile
-        fields = '__all__'
+        fields = ['email', 'name', 'handle', 'profile_picture', 'avatar_url']
+
+    def get_avatar_url(self, obj):
+        return obj.avatar_url
